acff0d6ea9f631e83b72173b8787eb39e31e85d3
david ullua <davidullua@ihopeit.com>
2023-06-16 20:49:20 +0800

    avoid error when sending the second message:TypeError: chatMessage._getType is not a function

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { Chroma } from 'langchain/vectorstores/chroma';
 import { makeChain } from '@/utils/makechain';
+import {BaseChatMessage, HumanChatMessage, AIChatMessage} from 'langchain/schema';
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,7 +10,19 @@ export default async function handler(
 ) {
   const { question, history } = req.body;
 
-  console.log('question', question);
+  let histories: BaseChatMessage[] = [];
+  history.forEach(hist => {
+    if(hist['type'] === 'human')  {
+      let req: BaseChatMessage = new HumanChatMessage(question);
+      histories.push(req);
+    } else if (hist['type'] === 'ai') {
+      let respond: BaseChatMessage = new AIChatMessage(question);
+      histories.push(respond);
+    }
+  });
+
+
+  console.log('question:', question);
 
   //only accept post requests
   if (req.method !== 'POST') {
@@ -40,13 +53,13 @@ export default async function handler(
     //Ask a question using chat history
     const response = await chain.call({
       question: sanitizedQuestion,
-      chat_history: history || [],
+      chat_history: histories || [],
     });
 
     console.log('response', response);
     res.status(200).json(response);
   } catch (error: any) {
-    console.log('error', error);
+    console.log('error:', error);
     res.status(500).json({ error: error.message || 'Something went wrong' });
   }
 }
