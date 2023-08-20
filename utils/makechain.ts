a3c3b1f523e42947a477edc4dcd777f93543d629
johame72 <johnhmerkel@gmail.com>
2023-08-20 10:59:39 -0500

    Initial commit

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,28 +2,46 @@ import { OpenAI } from 'langchain/llms/openai';
 import { Chroma } from 'langchain/vectorstores/chroma';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+// Enum for model names
+enum ModelName {
+  GPT3 = 'gpt-3.5-turbo-0613',
+  GPT4 = 'gpt-4'
+}
+
+const CONDENSE_PROMPT = `Given the conversation history and a follow-up question, rephrase the follow-up question to stand on its own while still referencing the necessary context from the conversation. 
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
-Standalone question:`;
+Rephrased standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+const QA_PROMPT = `You are a helpful AI assistant. Given the context below, answer the final question. If the answer isn't clear from the context, or if you don't know the answer, simply state that you don't know. 
 
 {context}
 
 Question: {question}
-Helpful answer in markdown:`;
+Your detailed answer in markdown format:`;
 
-export const makeChain = (vectorstore: Chroma) => {
-  const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo-0613', //change this to gpt-4 if you have access
-    //modelName: 'gpt-4', //change this to gpt-4 if you have access
-  });
+export const makeChain = (
+  vectorstore: Chroma,
+  temperature: number = .3, 
+  modelName: ModelName = ModelName.GPT4
+) => {
+  let model;
+
+  try {
+    model = new OpenAI({
+      temperature,
+      modelName
+    });
+  } catch (error) {
+    console.warn("Failed to use GPT-4, falling back to GPT-3: ", error);
+
+    model = new OpenAI({
+      temperature,
+      modelName: ModelName.GPT3
+    });
+  }
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
@@ -31,8 +49,9 @@ export const makeChain = (vectorstore: Chroma) => {
     {
       qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+      returnSourceDocuments: true,
     },
   );
+  
   return chain;
 };
